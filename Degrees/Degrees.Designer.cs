73ed53ead1a7bfcf01be25fcfb97c1651b20382c
PerryMartin <perry.martin@ocsbstudent.ca>
2015-10-26 13:27:20 -0400

    Hi

---
+++ Degrees/Degrees.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Degrees
+{
+    partial class Degrees
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPuch = new System.Windows.Forms.Button();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPuch
+            // 
+            this.btnPuch.Location = new System.Drawing.Point(12, 51);
+            this.btnPuch.Name = "btnPuch";
+            this.btnPuch.Size = new System.Drawing.Size(75, 23);
+            this.btnPuch.TabIndex = 0;
+            this.btnPuch.Text = "Calculate";
+            this.btnPuch.UseVisualStyleBackColor = true;
+            this.btnPuch.Click += new System.EventHandler(this.btnPuch_Click);
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(12, 25);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(100, 20);
+            this.txtBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(15, 8);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(140, 13);
+            this.lblInfo.TabIndex = 5;
+            this.lblInfo.Text = "Enter the degrees in celsius.";
+            // 
+            // Degrees
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 83);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.btnPuch);
+            this.Name = "Degrees";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPuch;
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ Degrees/Degrees.cs
@@ -0,0 +1,37 @@
+﻿// Andrew Mackinnon
+// October 26 2015
+// ICS3UR
+// Converts celsius to fahrenheit
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Degrees
+{
+    public partial class Degrees : Form
+    {
+        public void Converter (double temp)
+        {
+            double fahrenheit = (9 / 5) * temp + 32;
+            MessageBox.Show(Convert.ToString(fahrenheit) + " degrees is the temperature in fahrenheit");
+        }
+        public Degrees()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPuch_Click(object sender, EventArgs e)
+        {
+            Double degrees;
+
+            degrees = Convert.ToDouble(this.txtBox.Text);
+            Converter(degrees);
+        }
+    }
+}
